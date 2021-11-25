a028e16337e43d739b5debf449cb3499cd00a89c
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-11-25 16:32:40 +0300

    initial commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n):
+	if (n <= 0):
+		return []
+	triangle  = [[1]]
+	for i in range(n - 1):
+		row = []
+		for j in range(len(triangle[i]) + 1):
+			if (j - 1 < 0):
+				row.append(triangle[i][j])
+			elif (j == len(triangle[i])):
+				row.append(triangle[i][j - 1])
+			else:
+				row.append(triangle[i][j] + triangle[i][j - 1])
+		triangle.append(row)
+	return triangle
