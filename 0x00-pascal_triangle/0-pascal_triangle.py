94b3500d0e556c3d81e60929611a3f27690d6ce6
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-11-25 16:37:04 +0300

    pep8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,22 +4,23 @@
 	pascals triangle
 """
 
+
 def pascal_triangle(n):
-	"""
-		implementation of pascals triangle
-		returns a list of lists representing pascals triangle
-	"""
-	if (n <= 0):
-		return []
-	triangle  = [[1]]
-	for i in range(n - 1):
-		row = []
-		for j in range(len(triangle[i]) + 1):
-			if (j - 1 < 0):
-				row.append(triangle[i][j])
-			elif (j == len(triangle[i])):
-				row.append(triangle[i][j - 1])
-			else:
-				row.append(triangle[i][j] + triangle[i][j - 1])
-		triangle.append(row)
-	return triangle
+    """
+            implementation of pascals triangle
+            returns a list of lists representing pascals triangle
+    """
+    if (n <= 0):
+        return []
+    triangle = [[1]]
+    for i in range(n - 1):
+        row = []
+        for j in range(len(triangle[i]) + 1):
+            if (j - 1 < 0):
+                row.append(triangle[i][j])
+            elif (j == len(triangle[i])):
+                row.append(triangle[i][j - 1])
+            else:
+                row.append(triangle[i][j] + triangle[i][j - 1])
+        triangle.append(row)
+    return triangle
