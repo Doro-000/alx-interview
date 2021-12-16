340d713ee3dfa8616b790469d57b91796dc08cde
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-12-16 13:19:38 +0300

    a lil refactoring

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,24 +5,33 @@ log parsing
 
 import sys
 import re
+import signal
 
-regex = re.compile(
+
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
+
+if __name__ == "__main__":
+    regex = re.compile(
     r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)')  # nopep8
 
-
-def log_parse() -> None:
-    """
-    parses a log file
-    """
     line_count = 0
     log = {}
     log["file_size"] = 0
     log["code_frequency"] = {
         str(code): 0 for code in [
             200, 301, 400, 401, 403, 404, 405, 500]}
+
     try:
         for line in sys.stdin:
-            match = regex.fullmatch(line.strip())
+            line = line.strip()
+            match = regex.fullmatch(line)
             if (match):
                 line_count += 1
                 code = match.group(1)
@@ -36,19 +45,5 @@ def log_parse() -> None:
 
                 if (line_count % 10 == 0):
                     output(log)
-    except KeyboardInterrupt:
+    except Exception:
         output(log)
-
-
-def output(log: dict) -> None:
-    """
-    helper function to display stats
-    """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
-
-
-if __name__ == "__main__":
-    log_parse()
