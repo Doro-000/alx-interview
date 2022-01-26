f86165f6e5a0d8dc71f1df04d872026cc84e105e
Hizkyas Tekle <hizkyastekle@gmail.com>
2022-01-26 10:41:28 +0300

    docs

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+Coin change problem
+Given a pile of coins of different values, determine the
+fewest number of coins needed to meet a given amount total
+"""
+
 
 def makeChange(coins, total):
+    """
+    implementation
+    """
     if total <= 0:
         return 0
     ans = 0
